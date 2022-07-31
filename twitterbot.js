707b81a0c6e6189a7cc6176eeea370eaa807e4f1
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-07-31 17:27:09 -0300

    change second to day

--- twitterbot.js
+++ twitterbot.js
@@ -2,7 +2,7 @@ const rwClient = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
 const { formatDistanceStrict } = require('date-fns')
 
-const calcDaysUntil = () => formatDistanceStrict(new Date(), new Date('January 1, 2023 12:00:00'), { unit: "second" });
+const calcDaysUntil = () => formatDistanceStrict(new Date(), new Date('January 1, 2023 12:00:00'), { unit: "day" });
 const getDay = (day) => day.replace(/\D/g, "")
 
 const tweet = async () => {
